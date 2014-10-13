a85de9393a2831823dda97956ce911fb2ead9979
GongPA <david.chen@spsmail.cuny.edu>
2014-10-13 05:42:42 -0400

    Create task_02.py

---
+++ task_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Task 02: Dictionary Access
+"""
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BAND_NAMES = data.BANDS.keys()
